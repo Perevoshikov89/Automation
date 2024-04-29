0cb5d6a6f6ecafe838d5a99f26c05fff45d2a462
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-04-29 16:30:30 +0400

    изменен файл клик по кнопке без ID

---
+++ Lesson_5/Клик по кнопке без ID mozilla.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+# Установка и запуск драйвера Firefox
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+# Зайди на сайт
+driver.get("http://uitestingplayground.com/dynamicid")
+
+# Найди кнопку "синию кнопку" и кликни на нее
+
+button_with_dynamic_id = driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-primary"]')
+button_with_dynamic_id.click()
