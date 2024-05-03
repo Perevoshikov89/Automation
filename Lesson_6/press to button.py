c3907d38bf8dee10bafbd04c40c4e6cb8f28f9da
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-05-03 14:01:21 +0400

    исправлен файл press to button

--- Lesson_6/press to button.py
+++ Lesson_6/press to button.py
@@ -7,12 +7,13 @@ from time import sleep
 # Установка и запуск драйвера Firefox
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
-# Зайди на сайт, найди кнопку "синию кнопку" и кликни на нее
+# Настройка драйвера на ожидание
+driver.implicitly_wait(17)
+
+# Зайди на сайт, найди  "синию кнопку" и кликни на нее
 driver.get("http://uitestingplayground.com/ajax")
 blue_button = driver.find_element(By.CSS_SELECTOR, 'button.btn.btn-primary').click()
 
-# Жди 17 секунд
-sleep(17)
 
 # Найди появившийся элемент и выведи в консоль текст на элементе
 data = driver.find_element(By.CSS_SELECTOR, 'p[class="bg-success"]').text
