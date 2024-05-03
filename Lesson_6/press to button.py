e42468478e1699a81be7b37419c599d4d1689399
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-05-03 14:46:08 +0400

    исправлен файл waiting pic

--- Lesson_6/press to button.py
+++ Lesson_6/press to button.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
-from time import sleep
 
 # Установка и запуск драйвера Firefox
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
