f2100e7e436aca9723f4bc69324abe618061d8c2
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-05-04 23:50:05 +0400

    исправлен файл заполнение формы

--- Lesson_6.1/заполнение формы.py
+++ Lesson_6.1/заполнение формы.py
@@ -12,43 +12,33 @@ driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
 # В поле First name введи значение 'Иван'
-First_name= driver.find_element(By.CSS_SELECTOR, 'input[name="first-name"]')
-First_name.send_keys("Иван")
+First_name= driver.find_element(By.CSS_SELECTOR, 'input[name="first-name"]').send_keys("Иван")
 
 # В поле Last name введи значение 'Петров'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="last-name"]')
-input_password_field.send_keys("Петров")
+input_last_name_field = driver.find_element(By.CSS_SELECTOR, 'input[name="last-name"]').send_keys("Петров")
 
 # В поле Address  введи значение 'Ленина, 55-3'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="address"]')
-input_password_field.send_keys("Ленина, 55-3")
+input_address_field = driver.find_element(By.CSS_SELECTOR, 'input[name="address"]').send_keys("Ленина, 55-3")
 
 # В поле Email  введи значение 'test@skypro.com'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="e-mail"]')
-input_password_field.send_keys("test@skypro.com")
+input_email_field = driver.find_element(By.CSS_SELECTOR, 'input[name="e-mail"]').send_keys("test@skypro.com")
 
 # В поле phonenumber  введи значение '+7985899998787'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="phone"]')
-input_password_field.send_keys("+7985899998787")
+input_phonenumber_field = driver.find_element(By.CSS_SELECTOR, 'input[name="phone"]').send_keys("+7985899998787")
 
 # В поле city  введи значение 'Москва'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="city"]')
-input_password_field.send_keys("Москва")
+input_city_field = driver.find_element(By.CSS_SELECTOR, 'input[name="city"]').send_keys("Москва")
 
 # В поле country  введи значение 'Россия'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="country"]')
-input_password_field.send_keys("Россия")
+input_country_field = driver.find_element(By.CSS_SELECTOR, 'input[name="country"]').send_keys("Россия")
 
 # В поле Job position  введи значение 'QA'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="job-position"]')
-input_password_field.send_keys("QA")
+input_job_position_field = driver.find_element(By.CSS_SELECTOR, 'input[name="job-position"]').send_keys("QA")
 
 # В поле Company  введи значение 'SkyPro'
-input_password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="company"]')
-input_password_field.send_keys("SkyPro")
+input_company_field = driver.find_element(By.CSS_SELECTOR, 'input[name="company"]').send_keys("SkyPro")
 
 # Нажми кнопку Submit
-submit_button = driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-outline-primary mt-3"]').click()
+submit_button = driver.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
 
-
-driver(quit)
+driver.quit()
