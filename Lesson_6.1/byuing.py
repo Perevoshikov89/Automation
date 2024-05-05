7864e215f496e274ad6682123c1c68dbb15ae3de
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-05-05 23:29:58 +0400

    добавлены файлы с заданиями

---
+++ Lesson_6.1/byuing.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+# Установка и запуск драйвера Firefox
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+# Зайди на сайт
+driver.get("https://www.saucedemo.com/")
+
