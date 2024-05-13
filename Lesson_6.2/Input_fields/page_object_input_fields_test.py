c43d9f9ae253c5397b6d8526b7ac1dbf77c0cdbe
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-05-13 16:21:19 +0400

    внесены изменения

--- Lesson_6.2/Input_fields/page_object_input_fields_test.py
+++ Lesson_6.2/Input_fields/page_object_input_fields_test.py
@@ -4,19 +4,20 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_input_fields(self, driver):
-    self._driver = driver
-    self._driver.implicitly_wait(4)
+def test_input_fields():
+    browser = webdriver.Chrome()
+    main_page = MainPage(browser)
  # Проверь подсветку полей
-    zip_code_color = self._driver.find_element(By.CSS_SELECTOR, '[name="zip-code"]').value_of_css_property('background_color')
+    zip_code_color = browser.find_element(By.CSS_SELECTOR, '[name="zip-code"]').value_of_css_property('background_color')
     assert zip_code_color == ("#f8d7da")
     
     other_fields = ['#first_name', '#last_name', '#addres', '#city', '#country', '#e-mail', '#phone', '#job-position', '#company']
     for field in other_fields:
-    field_color = self._driver.find_element(By.CSS_SELECTOR, field). value_of_css_property('background_color')
-    assert field_color == ("#d1e7dd")
+        field_color = browser.find_element(By.CSS_SELECTOR, field). value_of_css_property('background_color')
+        assert field_color == ("#d1e7dd")
 
 
