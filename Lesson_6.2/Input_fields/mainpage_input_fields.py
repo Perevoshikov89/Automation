7062209552ebb4dd72a036cabf53092480cbdb62
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-05-14 21:45:39 +0400

    исправлены файлы для теста input_fields

--- Lesson_6.2/Input_fields/mainpage_input_fields.py
+++ Lesson_6.2/Input_fields/mainpage_input_fields.py
@@ -23,5 +23,13 @@ class MainPage:
         sleep(5)
         self._driver.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
         sleep(5)
-        
+
+    def color(self):
+        zip_code_color = self._driver.find_element(By.CSS_SELECTOR, '[name="zip-code"]').value_of_css_property('background_color')
+        assert zip_code_color == ("#f8d7da")
+    
+        other_fields = ['#first_name', '#last_name', '#addres', '#city', '#country', '#e-mail', '#phone', '#job-position', '#company']
+        for field in other_fields:
+            field_color = self._driver.find_element(By.CSS_SELECTOR, field). value_of_css_property('background_color')
+        assert field_color == ("#d1e7dd")
 
--- Lesson_6.2/Input_fields/page_object_input_fields_test.py
+++ Lesson_6.2/Input_fields/page_object_input_fields_test.py
@@ -13,13 +13,7 @@ def test_input_fields():
         service = Service(ChromeDriverManager().install()))
     
     main_page = MainPage(browser)
- # Проверь подсветку полей
-    zip_code_color = browser.find_element(By.CSS_SELECTOR, '[name="zip-code"]').value_of_css_property('background_color')
-    assert zip_code_color == ("#f8d7da")
-    
-    other_fields = ['#first_name', '#last_name', '#addres', '#city', '#country', '#e-mail', '#phone', '#job-position', '#company']
-    for field in other_fields:
-        field_color = browser.find_element(By.CSS_SELECTOR, field). value_of_css_property('background_color')
-        assert field_color == ("#d1e7dd")
+    main_page.color
 
+    browser.quit()
 
