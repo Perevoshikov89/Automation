983ff473095bbe94d3ee3cff0d6736f1a4c67501
Perevoshikov_89 <yuri.perevoshikov@gmail.com>
2024-05-24 13:46:40 +0400

    Изменен файл  test_empoyees

--- Lesson_8/test_employees.py
+++ Lesson_8/test_employees.py
@@ -1,12 +1,13 @@
 import requests
+import json
 from EmployeesApi import EmployeesApi
 
 api = EmployeesApi("https://x-clients-be.onrender.com")
 
 def test_add_new_employee():
     
-    name = "Sea products Co"
-    descr = "sea products"
+    name = "QA_masters"
+    descr = "masters_of_QA"
     result = api.create_company(name, descr)
     new_id = result["id"]
     
@@ -16,14 +17,14 @@ def test_add_new_employee():
     body = api.get_employees_list(companyId)
     len_before = len(body)
    
-    firstName = "Daria"
-    lastName = "Abramovich"
-    middleName = "Dmitrievna"
+    firstName = "Ivan"
+    lastName = "Ivanov"
+    middleName = "Ivanovich"
     company = companyId
-    email = "abramovichdash@mail.ru"
+    email = "ivanov_ivan@mail.ru"
     url = "string"
-    phone = "89670425734"
-    birthdate = "1991-11-24"
+    phone = "89991234567"
+    birthdate = "1990-01-01"
     isActive = True
     new_employee = api.create_employee(firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
     emp_id = new_employee["id"]
@@ -31,18 +32,18 @@ def test_add_new_employee():
     body = api.get_employees_list(companyId) 
     len_after = len(body)
     assert len_after - len_before == 1
-    assert body[-1]["firstName"] == "Daria"
-    assert body[-1]["lastName"] == "Abramovich"
-    assert body[-1]["middleName"] == "Dmitrievna"
+    assert body[-1]["firstName"] == "Ivan"
+    assert body[-1]["lastName"] == "Ivanov"
+    assert body[-1]["middleName"] == "Ivanovich"
     assert body[-1]["companyId"] == companyId
-    assert body[-1]["phone"] == "89670425734"
-    assert body[-1]["birthdate"] == "1991-11-24"
+    assert body[-1]["phone"] == "89991234567"
+    assert body[-1]["birthdate"] == "1990-01-01"
     assert body[-1]["isActive"] == True
     assert body[-1]["id"] == emp_id
 
 def test_get_employees_id():
-    name = "Aviasales"
-    descr = "авиакомпания"
+    name = "Motoparts"
+    descr = "Запчасти для мотоциклов"
     new_company = api.create_company(name, descr)
     new_id = new_company["id"]
   
@@ -52,14 +53,14 @@ def test_get_employees_id():
     body = api.get_employees_list(companyId)
     begin_list = len(body) 
    
-    firstName = "Oleg"
-    lastName = "Tinkoff"
-    middleName = "Alexandrovich"
+    firstName = "Moto"
+    lastName = "Motov"
+    middleName = "Akropovich"
     company = companyId
-    email = "tinkoff777@yandex.ru"
+    email = "motoparts@moto.ru"
     url = "string"
-    phone = "89035145997"
-    birthdate = "1971-06-04"
+    phone = "89992345678"
+    birthdate = "2000-01-01"
     isActive = True
     new_employee = api.create_employee(firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
     emp_id = new_employee["id"]
@@ -70,32 +71,32 @@ def test_get_employees_id():
  
     new_employee = api.get_employee(emp_id)
     employee_id = new_employee["id"]
-    assert body[-1]["firstName"] == "Oleg"
-    assert body[-1]["lastName"] ==  "Tinkoff"
-    assert body[-1]["middleName"] == "Alexandrovich"
+    assert body[-1]["firstName"] == "Moto"
+    assert body[-1]["lastName"] ==  "Motov"
+    assert body[-1]["middleName"] == "Akropovich"
     assert body[-1]["companyId"] == companyId
-    assert body[-1]["phone"] ==  "89035145997"
-    assert body[-1]["birthdate"] == "1971-06-04"
+    assert body[-1]["phone"] ==  "89992345678"
+    assert body[-1]["birthdate"] == "2000-01-01"
     assert body[-1]["isActive"] == True
     assert body[-1]["id"] == emp_id
 
 def test_patch_employee():
-    name = "McDonalds"
-    descr = "fast food"
+    name = "По ветру"
+    descr = "Одежда для мотоциклистов"
     new_company = api.create_company(name, descr)
     new_id = new_company["id"]
     
     new_company = api.get_company(new_id)
     companyId = new_company["id"]
     
-    firstName = "Vladimir"
-    lastName = "Arseniev"
-    middleName = "Konstantinovich"
+    firstName = "Ветер"
+    lastName = "Ветров"
+    middleName = "Ветрович"
     company = companyId
-    email = "vlars@mail.ru"
+    email = "po_vetru@mail.ru"
     url = "string"
-    phone = "89051112350"
-    birthdate = "1992-08-17"
+    phone = "89998765432"
+    birthdate = "2000-01-02"
     isActive = True
     new_employee = api.create_employee(firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
     emp_id = new_employee["id"]
@@ -105,11 +106,11 @@ def test_patch_employee():
     new_employee = api.get_employee(emp_id)
     employee_id = new_employee["id"]
 
-    new_email = "popov@mail.ru"
+    new_email = "po_vetru@mail.ru"
     new_url = "_Updated_"
     new_phone = "Updated"
     new_isActive = False
     edited = api.edit_employee(employee_id, new_email, new_url, new_phone, new_isActive)
-    assert edited["email"] == "popov@mail.ru"
+    assert edited["email"] == "po_vetru@mail.ru"
     assert edited["url"] == "_Updated_"
     assert edited["isActive"] == False
